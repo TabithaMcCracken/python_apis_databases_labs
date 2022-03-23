9901b1b9a215c03e3c948baca7e9528b3c6960e9
Tabitha <tabitha.r.mccracken@gmail.com>
2022-03-22 23:55:21 -0600

    ex 5

--- databases/Exercise_05.py
+++ databases/Exercise_05.py
@@ -22,4 +22,37 @@ To prevent this, you should add a check to see if the record already exists befo
 from pprint import pprint
 import sqlalchemy
 from secret import password
+import requests
+import json
 
+
+# Function to get task data
+def get_tasks():
+    task_url = "http://demo.codingnomads.co:8080/tasks_api/tasks"
+    task_request = requests.get(task_url)
+    data = task_request.text
+    parsed_json = json.loads(data)
+    return parsed_json
+
+# Function view all your tasks (GET)
+def view_tasks():
+    # Get task data
+    task_data = get_tasks()
+    # Get only "data"
+    user_data = task_data["data"]
+
+    userId_input = int(input("Please enter your user Id: \n"))
+
+    task_list = []
+    
+
+    for task_data in user_data:
+        task_access = task_data["userId"]
+        if task_access == userId_input:
+            task = task_data["name"]
+            task_list.append(task)
+            
+    print(f"Here is your task list: \n {task_list}")
+
+
+view_tasks()
