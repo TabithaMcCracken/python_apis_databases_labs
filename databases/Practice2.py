097e5be64c3cb29ca9f1a16f5eb7c1e08ccc10f7
Tabitha <tabitha.r.mccracken@gmail.com>
2022-03-31 11:13:28 -0600

    More Ex 5

--- databases/Practice2.py
+++ databases/Practice2.py
@@ -1,12 +1,12 @@
-# my_list = [1, 22, 3, 5, 42]
+my_list = [1, 22, 3, 5, 42]
 
-# new_list = []
-# for x in my_list:
-#     breakpoint()
-#     x = x+1
-#     new_list.append(x)
+new_list = []
+for x in my_list:
+    breakpoint()
+    x=x+1
+    new_list.append(x)
 
-# print(new_list)
+print(new_list)
 
 
 # a = 0
