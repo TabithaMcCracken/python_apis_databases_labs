70c5a643f2998555b6aebfe8757561197358df36
Tabitha <tabitha.r.mccracken@gmail.com>
2022-05-02 23:40:18 -0600

    more progress on ex 5

--- databases/Exercise_05.py
+++ databases/Exercise_05.py
@@ -42,28 +42,29 @@ def get_users():
     return parsed_json
 
 parsed_user_data = get_users()
-user_list = [] # Empty list for data
+
+api_user_list = [] # Empty list for data
 def make_user_list(): # Function for accesssing and appending "data"
     user_data = parsed_user_data['data'] # accessing only the 'data' dict
     for data in user_data: # going through all the data in the 'data' dict
-        user_list.append(data) # Changed 'email_access' to 'email_data'
+        api_user_list.append(data) # Changed 'email_access' to 'email_data'
 
 make_user_list() # Call the function
-#pprint(user_list) # data is list of dictionaries as "user_list"
+# pprint(api_user_list) # data is list of dictionaries as "user_list"
 
 
 
 # Function to get task data from API
-# def get_tasks():
-#     task_url = "http://demo.codingnomads.co:8080/tasks_api/tasks"
-#     task_request = requests.get(task_url)
-#     data = task_request.text
-#     parsed_json = json.loads(data)
-#     return parsed_json
+def get_tasks():
+    task_url = "http://demo.codingnomads.co:8080/tasks_api/tasks"
+    task_request = requests.get(task_url)
+    data = task_request.text
+    parsed_json = json.loads(data)
+    return parsed_json
 
 
-# task_data = get_tasks()
-# pprint(task_data)
+api_task_data = get_tasks()
+# pprint(api_task_data)
 
 
 
@@ -73,28 +74,36 @@ users_table = sqlalchemy.Table('users', metadata, autoload=True, autoload_with=e
 
 query = sqlalchemy.select([users_table])
 result_proxy = connection.execute(query)
-users_result_set = result_proxy.fetchall()
-#pprint(users_result_set)
+db_users_list = result_proxy.fetchall()
+# pprint(db_users_list)
+db_id_list = []
+for item in db_users_list:
+    db_id_list.append(item[0])
+# print(db_id_list)
 
 
-for item in user_list:
-    print (item["id"])
 
-# new_dict = []
+# Compare user ids in api_user_list to db_users_list
+# Create list of new user id's
+new_user_id_list = []
+for api_item in api_user_list:
+    # print(api_item['id'])
+    if api_item['id'] not in db_id_list:
+        new_user_id_list.append(api_item['id'])
+print(new_user_id_list)
 
-# for row in result_set:
-#     for user in user_list:
-#         if user != row:
-#             new_dict.append(user)
+# Filter api data to just new user id's data
+new_user_list = []
 
-# pprint(new_dict)
+for item in new_user_id_list:
+    for dics in api_user_list:
+        if dics.get("id") == item:
+            new_user_list.append(dics)
 
-
+print(new_user_list)
 
 
 # Put the API data into the 'users' table
-# users = sqlalchemy.Table('users', metadata, autoload=True, autoload_with=engine)
-# query = sqlalchemy.insert(users)
-# result_proxy = connection.execute(query, user_list)
-
-
+users = sqlalchemy.Table('users', metadata, autoload=True, autoload_with=engine)
+query = sqlalchemy.insert(users)
+result_proxy = connection.execute(query, new_user_list)
